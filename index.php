45f3260643ce310533b087d5d675bfb2ce97543a
Bo Skakke <bsk@berlingskemedia.dk>
2018-03-06 12:13:08 +0100

    banners

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Banner test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="/dist/styles/main.css">
 	<link rel="stylesheet" href="/dist/styles/demo.css">
 </head>
